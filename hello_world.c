d20018a57c106faa81e0b7bc92c099f5e2a40b37
kevin <Guzmank53@gmail.com>
2023-03-17 18:28:47 -0700

    guzmankev: first set of codes pushed

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>                  /*Stdio stands for Standard Input Output. Accepts input and output functionality*/
+#include <stdlib.h>                 
+
+int main()
+{
+    printf("Kevin, Guzman\n");       /*printf is a function. Here we put strings for this example*/
+    printf("My average grade is %.1f\n", 100.0);
+    return 0;
+}
